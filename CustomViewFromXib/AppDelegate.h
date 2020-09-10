9055ebd7e315ea95599f7573b24c56cd512298ce
shaoqing <xuesq@pvc123.com>
2020-09-10 15:30:50 +0800

    Initial Commit

---
+++ CustomViewFromXib/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomViewFromXib
+//
+//  Created by iMac on 2020/9/10.
+//  Copyright © 2020 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
